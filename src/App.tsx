5e56f0bb6a2706c821e57672ec27f6130f1d8456
GustavoVieiraSousa <gustavovieirasousa1v1@gmail.com>
2025-11-06 14:43:04 -0300

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-
+import {ThemeProvider} from 'styled-components'
+import {defaultTheme} from './Styles/Themes/default.ts'
 
 export function App() {
   
   return (
-    <>
+    <ThemeProvider theme={{defaultTheme}}>
 
-    </>
+    </ThemeProvider>
   )
 }
 
